8a30d08031dd860e37baba04412a42768ecf64dc
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2019-03-10 13:14:11 -0300

    Update uri1008.py

--- uri1008.py
+++ uri1008.py
@@ -1,8 +1,21 @@
-numfunc = int(input())
-hrtrab = int(input())
-valorhr = float(input())
+#  Orientado a Objetos por Shadowtampa
+#  POO version by Shadowtampa
 
-salario = float(hrtrab * valorhr)
 
-print("NUMBER = %d" %numfunc)
-print("SALARY = U$ %0.2f" %salario)
+class Funcionário:
+    númeroFuncionário = int()
+    horasTrabalhadas = int()
+    valorPorHora = float()
+    salário = float()
+
+
+funcionário = Funcionário()
+
+funcionário.númeroFuncionário = int(input())
+funcionário.horasTrabalhadas = int(input())
+funcionário.valorPorHora = float(input())
+
+funcionário.salário = funcionário.horasTrabalhadas * funcionário.valorPorHora
+
+print('NUMBER = {}\n'.format(funcionário.númeroFuncionário))
+print('SALARY = U${:.2f}'.format(funcionário.salário))
