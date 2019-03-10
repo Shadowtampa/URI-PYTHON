9676b91df5b16cb23a01c57f6ba51742736cdd82
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2019-03-10 12:51:50 -0300

    Update uri1006.py

--- uri1006.py
+++ uri1006.py
@@ -1,7 +1,19 @@
-n1 = float(input())
-n2 = float(input())
-n3 = float(input())
+#  Orientado a Objetos por Shadowtampa
+#  POO version by Shadowtampa
 
-soma = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / 10
 
-print("MEDIA = %0.1f" %soma)
+class Notas:
+    valor = float()
+    peso = float()
+
+
+a = Notas()
+b = Notas()
+c = Notas()
+a.peso, b.peso, c.peso = 2, 3, 5
+a.valor, b.valor, c.valor = float(input()), float(input()), float(input())
+result = ((a.valor * a.peso) + (b.valor * b.peso) + (c.valor * c.peso)) / (a.peso + b.peso + c.peso)  #  a * 3.5 + b * 7.5  /  11 <=> 3.5+7.5
+
+print('MEDIA = {:.1f}'.format(result))
+
+
