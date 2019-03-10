4aad62e6e4f3ef52ba9b7b2f9bd31f5b0e435b0d
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2019-03-10 14:48:38 -0300

    Update uri1014.py

--- uri1014.py
+++ uri1014.py
@@ -1,6 +1,16 @@
-distancia = int(input())
-combustivel = float(input())
+#  Orientado a Objetos por Shadowtampa
+#  POO version by Shadowtampa
 
-consumo = distancia / combustivel
 
-print("%0.3f km/l" %consumo)
+class Carro:
+    def __init__(self, distância, combustível):
+        self.distância = distância
+        self.combustível  = combustível
+
+    def Consumo(self):
+        return self.distância / self.combustível
+
+
+a, b = float(input()), float(input())
+carro = Carro(a, b)
+print('{:.3f} km/l'.format(carro.Consumo()))
