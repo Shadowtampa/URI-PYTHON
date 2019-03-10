18341e1c2761538917981a5afb37a634c317d7d9
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2019-03-10 13:19:22 -0300

    Update uri1009.py

--- uri1009.py
+++ uri1009.py
@@ -1,9 +1,19 @@
-nome = input()
-salfixo = float(input())
-qtdevendas = float(input())
+#  Orientado a Objetos por Shadowtampa
+#  POO version by Shadowtampa
 
-bonus = float(qtdevendas * (15/100))
 
-total = salfixo + bonus
+class vendedor:
+    nomeVendedor = str()
+    vendasTotais = float()
+    salário = float()
 
-print("TOTAL = R$ %0.2f" %total)
+
+vend = vendedor()  # criação do objeto 'vendedor' com nome 'vend'
+
+vend.nomeVendedor = str(input())
+vend.salário = float(input())
+vend.vendasTotais = float(input())
+
+result = vend.salário + (vend.vendasTotais * 0.15)  # n * 0,15 <=> 15% de n
+
+print('TOTAL = R$ {:.2f}'.format(result))
