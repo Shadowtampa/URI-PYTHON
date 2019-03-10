a150327c1c46d12420cd7d8ea750d4d54fc0f39d
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2019-03-10 12:18:25 -0300

    Update uri1010.py
    orientação a objetos

--- uri1010.py
+++ uri1010.py
@@ -1,3 +1,25 @@
+# ORIENTADO A OBJETOS por Shadowtampa
+class Produto:
+    ID = int(1)
+    quantidade = float()
+    valor = float()
+
+
+prod1 = Produto()
+prod2 = Produto()
+linha1 = input().split(" ")
+linha2 = input().split(" ")
+
+prod1.ID, prod1.quantidade, prod1.valor = linha1
+prod2.ID, prod2.quantidade, prod2.valor = linha2
+total = (float(prod1.quantidade)) * (float(prod1.valor)) + (float(prod2.quantidade)) * (float(prod2.valor))
+
+
+print('VALOR A PAGAR: R$ {:.2f}'.format(total)
+
+#########################################################################################################################################
+
+
 linha1 = input().split(" ")
 linha2 = input().split(" ")
 
@@ -7,3 +29,4 @@ cod2, qtde2, valor2 = linha2
 total = (int(qtde1) * float(valor1)) + (int(qtde2) * float(valor2))
 
 print("VALOR A PAGAR: R$ %0.2f" %total)
+
