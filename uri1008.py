7fad13b017e1dd3313c5af87135f1bbe1c38c20c
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2020-03-04 16:47:44 -0300

    Atualização 1008
    Joaozinho Pé Grande é um nome fictício

--- uri1008.py
+++ uri1008.py
@@ -1,8 +1,17 @@
-numfunc = int(input())
-hrtrab = int(input())
-valorhr = float(input())
+class funcionario:
+  def __init__(self, numero, horas, valorRecebido):
+    self.__numero = numero
+    self.__horas = horas
+    self.__valorRecebido = valorRecebido
 
-salario = float(hrtrab * valorhr)
+  def getWage(self):
+    return self.__horas * self.__valorRecebido
+  
+  def getNumber(self):
+    return self.__numero
 
-print("NUMBER = %d" %numfunc)
-print("SALARY = U$ %0.2f" %salario)
+JoaozinhoPeGrande = funcionario(int(input()), int(input()), float(input()))
+
+salario = JoaozinhoPeGrande.getWage()
+
+print("NUMBER =",JoaozinhoPeGrande.getNumber(), "\nSALARY = U$ %0.1f"%salario)
