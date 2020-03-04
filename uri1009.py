443720679e0119a0a4d5dd23dfa96b47af846afa
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2020-03-04 16:54:36 -0300

    Atualização 1009
    Joaozinho pé grande ataca novamente

--- uri1009.py
+++ uri1009.py
@@ -1,9 +1,16 @@
-nome = input()
-salfixo = float(input())
-qtdevendas = float(input())
+class vendedor:
+  comissao = 0.15
+  def __init__(self, nome, salarioFixo, totalVendas):
+    self.__nome = nome
+    self.__salarioFixo = salarioFixo 
+    self.__totalVendas = totalVendas
 
-bonus = float(qtdevendas * (15/100))
+  def getReceber(self):
+    return self.__salarioFixo + (self.__totalVendas * self.comissao) 
+  
+  def getNome(self):
+    return self.__nome
 
-total = salfixo + bonus
+JoaozinhoPeGrande = vendedor(input(), float(input()), float(input()))
 
-print("TOTAL = R$ %0.2f" %total)
+print("TOTAL = R$ %0.2f" %JoaozinhoPeGrande.getReceber())
