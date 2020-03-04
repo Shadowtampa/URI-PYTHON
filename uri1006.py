b60364365df6fba39db19c1c4249ec3041e06b5b
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2020-03-04 16:36:03 -0300

    Atualização 1006
    mais POO

--- uri1006.py
+++ uri1006.py
@@ -1,7 +1,21 @@
-n1 = float(input())
-n2 = float(input())
-n3 = float(input())
+class listaPonderada:
+  def __init__(self, valor, peso):
+    self.__valor = valor
+    self.__peso = peso 
 
-soma = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / 10
+  def getWeight(self):
+    return self.__peso 
 
-print("MEDIA = %0.1f" %soma)
+  def getWealth(self):
+    return self.__valor * self.__peso
+
+n1 = listaPonderada(float(input()),2)
+n2 = listaPonderada(float(input()),3)
+n3 = listaPonderada(float(input()),5)
+
+generalWeight = n1.getWeight() + n2.getWeight() + n3.getWeight()
+generalWealth = n1.getWealth() + n2.getWealth() + n3.getWealth()
+
+avg = generalWealth / generalWeight
+
+print("MEDIA = %0.1f"%avg)
