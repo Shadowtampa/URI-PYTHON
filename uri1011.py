2b4d0a7dace4706ac1f3cb1aced7cdf7943a41ea
Shadowtampa <44328281+Shadowtampa@users.noreply.github.com>
2020-03-04 17:23:01 -0300

    Atualização
    melhoras na classe, parâmetros e métodos

--- uri1011.py
+++ uri1011.py
@@ -1,7 +1,11 @@
-raio = int(input())
+class esfera:
+  def __init__(self, raio):
+    self.__raio = raio
+    self.__pi = 3.14159
+    self.__k = 4.0/3
+  def getVolume(self):
+    return self.__k * self.__pi * (self.__raio ** 3)
 
-pi = 3.14159
+bolitakkkk = esfera(float(input()))
 
-volume = float(4.0 * pi * (raio* raio * raio) / 3)
-
-print("VOLUME = %0.3f" %volume)
+print("VOLUME = %0.3f" %bolitakkkk.getVolume())
